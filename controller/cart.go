8ed98bf6c414267e695eea3dfe1cbf2a43948adb
Halim <oyurou.faiz@gmail.com>
2023-05-05 13:51:32 +0700

    Create cart.go

---
+++ controller/cart.go
@@ -0,0 +1,117 @@
+package controller
+
+import (
+	"mini_project/middleware"
+	"mini_project/model/payload"
+	"mini_project/usecase"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type CartController interface {
+	AddProductToCart(c echo.Context) error
+	RemoveProductFromCart(c echo.Context) error
+	GetCartByUserID(c echo.Context) error
+}
+
+type cartController struct {
+	cartUsecase usecase.CartUseCase
+}
+
+func NewCartController(
+	cartUsecase usecase.CartUseCase,
+) *cartController {
+	return &cartController{
+		cartUsecase: cartUsecase,
+	}
+}
+
+func (cc *cartController) AddProductToCart(c echo.Context) error {
+	userId, err := middleware.ExtractTokenUser(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, map[string]interface{}{
+			"message": "only users can access this feature",
+			"error":   err.Error(),
+		})
+	}
+
+	req := payload.AddProductToCartRequest{}
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid Request Payload",
+			"error":   err.Error(),
+		})
+	}
+	userID := userId
+	productID := req.ProductID
+	quantity := req.Quantity
+
+	err = cc.cartUsecase.AddProductToCart(userID, productID, quantity)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, map[string]interface{}{
+			"Error": err.Error(),
+		})
+	}
+	return c.JSON(http.StatusCreated, map[string]interface{}{
+		"message": "success Add Product to Cart",
+	})
+}
+
+func (cc *cartController) RemoveProductFromCart(c echo.Context) error {
+	userId, err := middleware.ExtractTokenUser(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, map[string]interface{}{
+			"message": "only users can access this feature",
+			"error":   err.Error(),
+		})
+	}
+
+	req := payload.AddProductToCartRequest{}
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid Request Payload",
+			"error":   err.Error(),
+		})
+	}
+	userID := userId
+	productID := req.ProductID
+	quantity := req.Quantity
+
+	err = cc.cartUsecase.RemoveProductFromCart(userID, productID, quantity)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, map[string]interface{}{
+			"Error": err.Error(),
+		})
+	}
+	return c.JSON(http.StatusCreated, map[string]interface{}{
+		"message": "success Remove Product from Cart",
+	})
+}
+
+func (cc *cartController) GetCartByUserID(c echo.Context) error {
+	userID, err := middleware.ExtractTokenUser(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, map[string]interface{}{
+			"message": "only users can access this feature",
+			"error":   err.Error(),
+		})
+	}
+
+	cart, err := cc.cartUsecase.GetCartByUserID(userID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, map[string]interface{}{
+			"message": "Cart Not Found",
+			"error":   err.Error(),
+		})
+	}
+	if cart.UserID != userID {
+		return echo.NewHTTPError(http.StatusUnauthorized, map[string]interface{}{
+			"message": "Unauthorized access to cart",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Success Get Cart",
+		"Cart":    cart,
+	})
+}
