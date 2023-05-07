c136ba375df13adadd1cbe723a7e9c57148fb71b
Halim <oyurou.faiz@gmail.com>
2023-05-07 15:38:47 +0700

    Update cart.go

--- model/cart.go
+++ model/cart.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type Cart struct {
 	gorm.Model
-	UserID     uint          `json:"user_id"`
-	TotalPrice uint          `json:"total_price"`
-	Status     string        `json:"status"`
-	CartItems  []CartProduct `json:"cart_items"`
+	UserID       uint          `json:"user_id"`
+	TotalPrice   uint          `json:"total_price"`
+	Status       string        `json:"status"`
+	CartProducts []CartProduct `json:"cart_products"`
 }
 
 type CartProduct struct {
