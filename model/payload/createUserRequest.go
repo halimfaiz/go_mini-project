8c861ae96ab47e369bfdf640d57922eed1f260af
Halim <oyurou.faiz@gmail.com>
2023-05-07 15:39:13 +0700

    Update createUserRequest.go

--- model/payload/createUserRequest.go
+++ model/payload/createUserRequest.go
@@ -1,9 +1,10 @@
 package payload
 
 type CreateUserRequest struct {
-	Name     string `json:"name" form:"name" validate:"required"`
-	Email    string `json:"email" form:"email" validate:"required,email"`
-	Password string `json:"password" form:"password" validate:"required,min=6"`
-	Address  string `json:"address" form:"address" validate:"required"`
-	Phone    string `json:"phone" form:"phone" validate:"required"`
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+	Address  string `json:"address" validate:"required"`
+	Phone    string `json:"phone" validate:"required"`
+	Role     string `json:"role"`
 }
