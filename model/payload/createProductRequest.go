60072f65133c077e8b0e98042124fddd2257e4ef
Halim <oyurou.faiz@gmail.com>
2023-05-09 22:12:56 +0700

    Update Logic

--- model/payload/createProductRequest.go
+++
@@ -1,8 +0,0 @@
-package payload
-
-type CreateProductRequest struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Price       uint   `json:"price"`
-	Stock       uint   `json:"stock"`
-}
